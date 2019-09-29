49140888bb9b6bd0b707263c24028b4c03282076
Laiming Huang <hicleler@berkeley.edu>
2019-09-28 21:53:49 -0700

    sketch

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ import App from './App';
 import {name as appName} from './app.json';
 import {Navigation} from "react-native-navigation";
 import HelloWorldApp from "./src/features/signup/components/signup";
-
+import {} from './src/features/signup/components/signup/index'
 AppRegistry.registerComponent(appName, () => App);
 AppRegistry.registerComponent('HelloWorldApp', () => HelloWorldApp);
-
+AppRegistry.registerComponent('signup', ()=>LoginPage)
 
 Navigation.events().registerAppLaunchedListener(() => {
     Navigation.setRoot({
--- src/features/signup/components/signup/index.js
+++ src/features/signup/components/signup/index.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { styles } from "./styles";
+import { field, input} from "src/styles/authentication"
 import {constants} from "../../constants"
 export default class HelloWorldApp extends Component {
     render() {
         return (
             <View style={styles.background}>
                 <Text style={styles.textHeader}>{constants.title}</Text>
-                <Text style={styles.fillName}>{constants.fillName}</Text>
-                <Text style={styles.lineStyle}> ——————————————————</Text>
+                <Text style={field}>{constants.fillName}</Text>
+
             </View>
 
         );
